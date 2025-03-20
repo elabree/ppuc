820c4e4545c9cb01ab5452ba637eb720b43a8831
Markus Kalkbrenner <git@kalki.de>
2025-03-20 20:26:11 +0100

    added missing deps

--- platforms/linux/x64/external.sh
+++ platforms/linux/x64/external.sh
@@ -192,9 +192,12 @@ cp libdmdutil/libdmdutil/third-party/include/serum.h ../third-party/include/
 cp libdmdutil/libdmdutil/third-party/include/serum-decode.h ../third-party/include/
 cp -a libdmdutil/libdmdutil/third-party/runtime-libs/linux/x64/libserialport.{so,so.*} ../third-party/runtime-libs/linux-x64/
 cp -a libdmdutil/libdmdutil/third-party/runtime-libs/linux/x64/libpupdmd.{so,so.*} ../third-party/runtime-libs/linux-x64/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/linux/x64/libpupdmd.{dylib,*.dylib} ../third-party/runtime-libs/linux-x64/
 cp libdmdutil/libdmdutil/third-party/include/pupdmd.h ../third-party/include/
 cp -a libdmdutil/libdmdutil/third-party/runtime-libs/linux/x64/libsockpp.{so,so.*} ../third-party/runtime-libs/linux-x64/
 cp libdmdutil/libdmdutil/third-party/runtime-libs/linux/x64/libcargs.so ../third-party/runtime-libs/linux-x64/
+cp -r libdmdutil/libdmdutil/third-party/include/sockpp ../third-party/include/
+cp libdmdutil/libdmdutil/third-party/include/cargs.h ../third-party/include/
 
 cp libppuc/libppuc/src/PPUC.h ../third-party/include/
 cp libppuc/libppuc/src/PPUC_structs.h ../third-party/include/
