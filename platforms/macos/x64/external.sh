381f6d96697abf44757109c6834a72db10d51fc1
Markus Kalkbrenner <git@kalki.de>
2025-04-26 21:04:05 +0200

    more linux libs

--- platforms/macos/x64/external.sh
+++ platforms/macos/x64/external.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-LIBOPENAL_SHA=d3875f333fb6abe2f39d82caca329414871ae53b
-LIBPINMAME_SHA=c69f68aca1fe28d5bb65ab10a17c09fb2593d57b
-LIBPPUC_SHA=2bb464dd10e37649e0ec3321edfaf71b4c1d3216
-LIBDMDUTIL_SHA=c7b28ff9b26d206820f438a54c9bc89171a3ae02
+source ./platforms/config.sh
 
 NUM_PROCS=$(sysctl -n hw.ncpu)
 
 echo "Building libraries..."
-echo "  LIBOPENAL_SHA: ${LIBOPENAL_SHA}"
-echo "  LIBPINMAME_SHA: ${LIBPINMAME_SHA}"
+echo "  SDL_SHA: ${SDL_SHA}"
+echo "  SDL_IMAGE_SHA: ${SDL_IMAGE_SHA}"
+echo "  PINMAME_SHA: ${PINMAME_SHA}"
 echo "  LIBPPUC_SHA: ${LIBPPUC_SHA}"
 echo "  LIBDMDUTIL_SHA: ${LIBDMDUTIL_SHA}"
 echo ""
-
-if [ -z "${BUILD_TYPE}" ]; then
-   BUILD_TYPE="Release"
-fi
 
 if [ -z "${CACHE_DIR}" ]; then
    CACHE_DIR="external/cache/${BUILD_TYPE}"
@@ -33,87 +27,185 @@ mkdir -p external ${CACHE_DIR}
 cd external
 
 #
-# libdmdutil
+# build SDL3, SDL3_image
 #
 
-CACHE_NAME="libdmdutil-${LIBDMDUTIL_SHA}"
+SDL3_EXPECTED_SHA="${SDL_SHA}-${SDL_IMAGE_SHA}"
+SDL3_FOUND_SHA="$([ -f SDL3/cache.txt ] && cat SDL3/cache.txt || echo "")"
 
-if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
-    rm -f ../${CACHE_DIR}/libdmdutil-*.cache
-    rm -rf libdmdutil-*
-    curl -sL https://github.com/PPUC/libdmdutil/archive/${LIBDMDUTIL_SHA}.zip -o libdmdutil.zip
-    unzip libdmdutil.zip
-    cd libdmdutil-${LIBDMDUTIL_SHA}
-    cp -r include/DMDUtil ../../third-party/include/
-    BUILD_TYPE=${BUILD_TYPE} platforms/macos/x64/external.sh
-    cp -a third-party/. ../../third-party
-    cmake -DPLATFORM=macos -DARCH=x64 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-    cmake --build build -- -j${NUM_PROCS}
-    cp -P build/*.dylib ../../third-party/runtime-libs/macos/x64/
-    cd ..
-    touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+if [ "${SDL3_EXPECTED_SHA}" != "${SDL3_FOUND_SHA}" ]; then
+   echo "Building SDL3. Expected: ${SDL3_EXPECTED_SHA}, Found: ${SDL3_FOUND_SHA}"
+
+   rm -rf SDL3
+   mkdir SDL3
+   cd SDL3
+
+   curl -sL https://github.com/libsdl-org/SDL/archive/${SDL_SHA}.tar.gz -o SDL-${SDL_SHA}.tar.gz
+   tar xzf SDL-${SDL_SHA}.tar.gz
+   mv SDL-${SDL_SHA} SDL
+   cd SDL
+   cmake \
+      -DSDL_SHARED=ON \
+      -DSDL_STATIC=OFF \
+      -DSDL_TEST_LIBRARY=OFF \
+      -DSDL_OPENGLES=OFF \
+      -DCMAKE_OSX_ARCHITECTURES=x64 \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=14.0 \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   cd ..
+
+   curl -sL https://github.com/libsdl-org/SDL_image/archive/${SDL_IMAGE_SHA}.tar.gz -o SDL_image-${SDL_IMAGE_SHA}.tar.gz
+   tar xzf SDL_image-${SDL_IMAGE_SHA}.tar.gz
+   mv SDL_image-${SDL_IMAGE_SHA} SDL_image
+   cd SDL_image
+   ./external/download.sh
+   cmake \
+      -DBUILD_SHARED_LIBS=ON \
+      -DSDLIMAGE_SAMPLES=OFF \
+      -DSDLIMAGE_DEPS_SHARED=ON \
+      -DSDLIMAGE_VENDORED=ON \
+      -DSDLIMAGE_AVIF=OFF \
+      -DSDLIMAGE_WEBP=OFF \
+      -DSDL3_DIR=../SDL/build \
+      -DCMAKE_OSX_ARCHITECTURES=x64 \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=14.0 \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   cd ..
+
+   echo "$SDL3_EXPECTED_SHA" > cache.txt
+
+   cd ..
+fi
+
+LIBDMDUTIL_EXPECTED_SHA="${LIBDMDUTIL_SHA}"
+LIBDMDUTIL_FOUND_SHA="$([ -f libdmdutil/cache.txt ] && cat libdmdutil/cache.txt || echo "")"
+
+if [ "${LIBDMDUTIL_EXPECTED_SHA}" != "${LIBDMDUTIL_FOUND_SHA}" ]; then
+   echo "Building libdmdutil. Expected: ${LIBDMDUTIL_EXPECTED_SHA}, Found: ${LIBDMDUTIL_FOUND_SHA}"
+
+   rm -rf libdmdutil
+   mkdir libdmdutil
+   cd libdmdutil
+
+   curl -sL https://github.com/vpinball/libdmdutil/archive/${LIBDMDUTIL_SHA}.tar.gz -o libdmdutil-${LIBDMDUTIL_SHA}.tar.gz
+   tar xzf libdmdutil-${LIBDMDUTIL_SHA}.tar.gz
+   mv libdmdutil-${LIBDMDUTIL_SHA} libdmdutil
+   cd libdmdutil
+   ./platforms/macos/x64/external.sh
+   cmake \
+      -DPLATFORM=macos \
+      -DARCH=x64 \
+      -DBUILD_STATIC=OFF \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   cd ..
+
+   echo "$LIBDMDUTIL_EXPECTED_SHA" > cache.txt
+
+   cd ..
 fi
 
 #
-# libopenal
+# pinamame
 #
 
-CACHE_NAME="openal-soft-${LIBOPENAL_SHA}"
+PINMAME_EXPECTED_SHA="${PINMAME_SHA}"
+PINMAME_FOUND_SHA="$([ -f pinmame/cache.txt ] && cat pinmame/cache.txt || echo "")"
 
-if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
-    rm -f ../${CACHE_DIR}/openal-soft-*.cache
-    rm -rf openal-soft-*
-    curl -sL https://github.com/kcat/openal-soft/archive/${LIBOPENAL_SHA}.zip -o openal-soft.zip
-    unzip openal-soft
-    cd openal-soft-${LIBOPENAL_SHA}
-    cp -r include/AL ../../third-party/include/
-    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_OSX_ARCHITECTURES=x86_64 -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DALSOFT_INSTALL_EXAMPLES=OFF -DALSOFT_INSTALL_UTILS=OFF -B build
-    cmake --build build -- -j${NUM_PROCS}
-    cp -P build/libopenal*.dylib ../../third-party/runtime-libs/macos/x64/
-    cd ..
-    touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
-fi
+if [ "${PINMAME_EXPECTED_SHA}" != "${PINMAME_FOUND_SHA}" ]; then
+   echo "Building libpinmame. Expected: ${PINMAME_EXPECTED_SHA}, Found: ${PINMAME_FOUND_SHA}"
 
-#
-# libpiname
-#
+   rm -rf pinmame
+   mkdir pinmame
+   cd pinmame
 
-CACHE_NAME="pinmame-${LIBPINMAME_SHA}"
+   curl -sL https://github.com/vpinball/pinmame/archive/${PINMAME_SHA}.tar.gz -o pinmame-${PINMAME_SHA}.tar.gz
+   tar xzf pinmame-${PINMAME_SHA}.tar.gz
+   mv pinmame-${PINMAME_SHA} pinmame
+   cd pinmame
+   cp cmake/libpinmame/CMakeLists.txt .
+   cmake \
+      -DPLATFORM=macos \
+      -DARCH=x64 \
+      -DBUILD_STATIC=OFF \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   cd ..
 
-if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
-    rm -f ../${CACHE_DIR}/pinmame-*.cache
-    rm -rf pinmame-*
-    curl -sL https://github.com/vpinball/pinmame/archive/${LIBPINMAME_SHA}.zip -o pinmame.zip
-    unzip pinmame.zip
-    cd pinmame-${LIBPINMAME_SHA}
-    cp src/libpinmame/libpinmame.h ../../third-party/include/
-    cp cmake/libpinmame/CMakeLists.txt CMakeLists.txt
-    cmake -DPPUC_SUPPORT=1 -DPLATFORM=macos -DARCH=x64 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-    cmake --build build -- -j${NUM_PROCS}
-    cp -P build/libpinmame*.dylib ../../third-party/runtime-libs/macos/x64/
-    cd ..
-    touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+   echo "$PINMAME_EXPECTED_SHA" > cache.txt
+
+   cd ..
 fi
 
 #
 # libppuc
 #
 
-CACHE_NAME="libppuc-${LIBPPUC_SHA}"
+LIBPPUC_EXPECTED_SHA="${LIBPPUC_SHA}"
+LIBPPUC_FOUND_SHA="$([ -f libppuc/cache.txt ] && cat libppuc/cache.txt || echo "")"
 
-if [ ! -f "../${CACHE_DIR}/${CACHE_NAME}.cache" ]; then
-    rm -f ../${CACHE_DIR}/libppuc-*.cache
-    rm -rf libppuc-*
-    curl -sL https://github.com/PPUC/libppuc/archive/${LIBPPUC_SHA}.zip -o libppuc.zip
-    unzip libppuc.zip
-    cd libppuc-${LIBPPUC_SHA}
-    cp src/PPUC.h ../../third-party/include/
-    cp src/PPUC_structs.h ../../third-party/include/
-    BUILD_TYPE=${BUILD_TYPE} platforms/macos/x64/external.sh
-    cp -a third-party/. ../../third-party
-    cmake -DPLATFORM=macos -DARCH=x64 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-    cmake --build build -- -j${NUM_PROCS}
-    cp -P build/libppuc*.dylib ../../third-party/runtime-libs/macos/x64/
-    cd ..
-    touch "../${CACHE_DIR}/${CACHE_NAME}.cache"
+if [ "${LIBPPUC_EXPECTED_SHA}" != "${LIBPPUC_FOUND_SHA}" ]; then
+   echo "Building libppuc. Expected: ${LIBPPUC_EXPECTED_SHA}, Found: ${LIBPPUC_FOUND_SHA}"
+
+   rm -rf libppuc
+   mkdir libppuc
+   cd libppuc
+
+   curl -sL https://github.com/PPUC/libppuc/archive/${LIBPPUC_SHA}.tar.gz -o libppuc-${LIBPPUC_SHA}.tar.gz
+   tar xzf libppuc-${LIBPPUC_SHA}.tar.gz
+   mv libppuc-${LIBPPUC_SHA} libppuc
+   cd libppuc
+
+   BUILD_TYPE=${BUILD_TYPE} platforms/macos/x64/external.sh
+
+   cmake \
+      -DPLATFORM=macos \
+      -DARCH=x64 \
+      -DBUILD_STATIC=OFF \
+      -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+      -B build
+   cmake --build build -- -j${NUM_PROCS}
+   cd ..
+
+   echo "$LIBPPUC_EXPECTED_SHA" > cache.txt
+
+   cd ..
 fi
+
+cp -a SDL3/SDL/build/libSDL3.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp -r SDL3/SDL/include/SDL3 ../third-party/include/
+
+cp -a SDL3/SDL_image/build/libSDL3_image.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp -r SDL3/SDL_image/include/SDL3_image ../third-party/include/
+
+cp -a pinmame/pinmame/build/libpinmame.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp pinmame/pinmame/src/libpinmame/libpinmame.h ../third-party/include/
+#cp pinmame/pinmame/src/libpinmame/pinmamedef.h ../third-party/include/
+
+cp -a libdmdutil/libdmdutil/build/libdmdutil.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp -r libdmdutil/libdmdutil/include/DMDUtil ../third-party/include/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libzedmd.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp libdmdutil/libdmdutil/third-party/include/ZeDMD.h ../third-party/include/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libserum.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp libdmdutil/libdmdutil/third-party/include/serum.h ../third-party/include/
+cp libdmdutil/libdmdutil/third-party/include/serum-decode.h ../third-party/include/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libserialport.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libpupdmd.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp libdmdutil/libdmdutil/third-party/include/pupdmd.h ../third-party/include/
+cp -a libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libsockpp.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp libdmdutil/libdmdutil/third-party/runtime-libs/macos/x64/libcargs.dylib ../third-party/runtime-libs/macos-x64/
+cp -r libdmdutil/libdmdutil/third-party/include/sockpp ../third-party/include/
+cp libdmdutil/libdmdutil/third-party/include/cargs.h ../third-party/include/
+
+cp libppuc/libppuc/src/PPUC.h ../third-party/include/
+cp libppuc/libppuc/src/PPUC_structs.h ../third-party/include/
+cp -r libppuc/libppuc/third-party/include/yaml-cpp ../third-party/include/
+cp -r libppuc/libppuc/third-party/include/io-boards ../third-party/include/
+cp -a libppuc/libppuc/build/libppuc.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
+cp -a libppuc/libppuc/third-party/runtime-libs/macos/x64/libyaml-cpp.{dylib,*.dylib} ../third-party/runtime-libs/macos-x64/
