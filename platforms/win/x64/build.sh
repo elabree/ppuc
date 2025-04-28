7fbac2390493cedab320d7fabccf7969ebe3f3c5
Markus Kalkbrenner <git@kalki.de>
2025-04-28 21:26:58 +0200

    renamed ppuc_pinamme to ppuc-pinmame

--- platforms/win/x64/build.sh
+++ platforms/win/x64/build.sh
@@ -8,11 +8,11 @@ fi
 
 BUILD_TYPE=${BUILD_TYPE} ./platforms/win/x64/external.sh
 
-cmake -G "Visual Studio 17 2022" -DPLATFORM=win -DARCH=x64 -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -B build
-cmake --build build
+cmake -G "Visual Studio 17 2022" -DPLATFORM=win -DARCH=x64 -B build
+cmake --build build --config ${BUILD_TYPE}
 
 rm -rf ppuc
 mkdir ppuc
 
-cp build/ppuc_pinmame ppuc/
+cp build/${BUILD_TYPE}/ppuc_pinmame ppuc/
 cp -P third-party/runtime-libs/win/x64/*.dll ppuc/
